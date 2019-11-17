cab50edcd88f5d759f4632ec99c373f9502c912e
gabisch18 <50716756+gabisch18@users.noreply.github.com>
2019-11-17 16:16:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const mainPlayArea = document.getElementById("main-play-area")
 const doggo = document.getElementById("player")
+const bones = ['images/bone.png', 'images/bone2.png', 'images/bone3.png']
+
+let boneInterval
+playGame()
 
 function letDoggoMove(event) {
   if (event.key === "ArrowLeft") {
@@ -16,7 +21,7 @@ function letDoggoMove(event) {
 
 function moveLeft() {
   let leftPosition = window.getComputedStyle(doggo).getPropertyValue('left')
-  if (doggo.style.left === "10px") {
+  if (doggo.style.left === "5px") { //fix
     return
   } else {
     let position = parseInt(leftPosition)
@@ -28,7 +33,7 @@ function moveLeft() {
 
 function moveRight() {
   let leftPosition = window.getComputedStyle(doggo).getPropertyValue('left')
-  if (doggo.style.left === "820px") {
+  if (doggo.style.left === "800px") {//fix
     return
   } else {
     let position = parseInt(leftPosition)
@@ -37,4 +42,32 @@ function moveRight() {
   }
 }
 
-window.addEventListener("keydown", letDoggoMove)
+function createBone() {
+  let newBone = document.createElement('img')
+  let boneSpriteImg = bones[Math.floor(Math.random()*bones.length)]
+  newBone.src = boneSpriteImg
+  newBone.classList.add('bone')
+  newBone.style.top = '5px'
+  newBone.style.left = `${Math.floor(Math.random() * 889) + 30}px`
+  mainPlayArea.appendChild(newBone)
+  moveBone(newBone)
+}
+
+
+function moveBone(bone) {
+  let moveBoneInterval = setInterval(() => {
+    let yPosition = parseInt(window.getComputedStyle(bone).getPropertyValue('top'))
+    if (yPosition >= 450) {
+        bone.remove()
+    } else {
+        bone.style.top = `${yPosition + 4}px`
+    }
+  }, 30)
+}
+
+//window.addEventListener("keydown", letDoggoMove)
+
+function playGame() {
+  window.addEventListener("keydown", letDoggoMove)
+  boneInterval = setInterval(() => { createBone() }, 2100)
+}
