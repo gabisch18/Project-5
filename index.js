a8375a61fc87898334f6b55aa9d5bb8f505e118e
gabisch18 <50716756+gabisch18@users.noreply.github.com>
2019-11-19 16:28:19 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function letDoggoMove(event) {
 
 function moveLeft() {
   let leftPosition = window.getComputedStyle(doggo).getPropertyValue('left')
-  if (doggo.style.left === "5px") { //fix
+  if (parseInt(doggo.style.left) <= 10) {
     return
   } else {
     let position = parseInt(leftPosition)
@@ -34,7 +34,7 @@ function moveLeft() {
 
 function moveRight() {
   let leftPosition = window.getComputedStyle(doggo).getPropertyValue('left')
-  if (doggo.style.left === "800px") {//fix
+  if (parseInt(doggo.style.left) >= 800) {
     return
   } else {
     let position = parseInt(leftPosition)
@@ -49,21 +49,45 @@ function createBone() {
   newBone.src = boneSpriteImg
   newBone.classList.add('bone')
   newBone.style.top = '5px'
-  newBone.style.left = `${Math.floor(Math.random() * 889)}px`
+  newBone.style.left = `${Math.floor(Math.random() * 850)}px`
   mainPlayArea.appendChild(newBone)
   moveBone(newBone)
 }
 
 
 function moveBone(bone) {
+    var vel = 3;
+    var acc = 0.1;
   let moveBoneInterval = setInterval(() => {
     let yPosition = parseInt(window.getComputedStyle(bone).getPropertyValue('top'))
+    if (checkCollision(bone, doggo)) {
+        console.log("true collision");
+        bone.remove();
+        scoreCounter.innerText = parseInt(scoreCounter.innerText) + 1;
+    }
     if (yPosition >= 450) {
-        bone.remove()
+        bone.remove();
     } else {
-        bone.style.top = `${yPosition + 4}px`
-    }//check collision in here
+        vel += acc;
+        bone.style.top = `${yPosition + vel}px`
+    }//check collision in here and keep score
   }, 30)
+}
+
+function checkCollision(bone, doggo) {
+  let boneLeft = parseInt(bone.style.left)
+  let boneTop = parseInt(bone.style.top)
+  let boneBottom = boneTop + 35
+  let doggoTop = parseInt(doggo.style.top)
+  let doggoBottom = doggoTop + 80
+  let doggoLeft = parseInt(doggo.style.left)
+  if (boneTop < doggoBottom && boneBottom > doggoTop && boneLeft < doggoLeft + 80 && boneLeft + 50 >doggoLeft) {
+      console.log("collision true");
+      return true
+  } else {
+      console.log("collision false");
+      return false
+  }
 }
 
 //window.addEventListener("keydown", letDoggoMove)
